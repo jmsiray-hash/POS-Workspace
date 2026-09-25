32424d26b4fa8e016e347dc92e150bd97c872011
Juraly Siray <your-email@example.com>
2026-09-26 01:20:15 +0800

    Fix Aiven SSL configuration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,9 @@ class Database extends Config
         'charset'      => 'utf8mb4',
         'DBCollat'     => 'utf8mb4_general_ci',
         'swapPre'      => '',
-        'encrypt'      => true,
+        'encrypt'      => [
+            'ssl_verify' => false,
+        ],
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
