f6d3a52c8418161ca1f7f97b57a2505155f403dc
Juraly Siray <your-email@example.com>
2026-09-26 00:10:49 +0800

    Remove duplicate default database property declaration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -4,18 +4,16 @@ namespace Config;
 
 use CodeIgniter\Database\Config;
 
-/**
- * Database Configuration
- */
 class Database extends Config
 {
     /**
-     * The directory that holds the Migrations and Seeds directories.
+     * The directory that holds the Migrations
+     * and Seeds directories.
      */
     public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
 
     /**
-     * Lets you choose which connection group to use if no other is specified.
+     * Database connection group to use if none specified.
      */
     public string $defaultGroup = 'default';
 
@@ -28,7 +26,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'mysql-3af8d409-gsiray49-3918.k.aivencloud.com',
         'username'     => 'avnadmin',
-        'password'     => '',
+        'password'     => 'AVNS_6oduRy3fzhjOW0yOzkn',
         'database'     => 'defaultdb',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
@@ -37,7 +35,7 @@ class Database extends Config
         'charset'      => 'utf8mb4',
         'DBCollat'     => 'utf8mb4_general_ci',
         'swapPre'      => '',
-        'encrypt'      => true, // Naka-ENABLE para sa Aiven SSL
+        'encrypt'      => true,
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
@@ -56,27 +54,27 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-  public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'mysql-3af8d409-gsiray49-3918.k.aivencloud.com',
-        'username'     => 'avnadmin',
-        'password'     => '',
-        'database'     => 'defaultdb',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => true,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 14702,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
+    public array $tests = [
+        'DSN'         => '',
+        'hostname'    => '127.0.0.1',
+        'username'    => '',
+        'password'    => '',
+        'database'    => ':memory:',
+        'DBDriver'    => 'SQLite3',
+        'DBPrefix'    => 'db_',
+        'pConnect'    => false,
+        'DBDebug'     => true,
+        'charset'     => 'utf8mb4',
+        'DBCollat'    => '',
+        'swapPre'     => '',
+        'encrypt'     => false,
+        'compress'    => false,
+        'strictOn'    => false,
+        'failover'    => [],
+        'port'        => 3306,
+        'foreignKeys' => true,
+        'busyTimeout' => 1000,
+        'dateFormat'  => [
             'date'     => 'Y-m-d',
             'datetime' => 'Y-m-d H:i:s',
             'time'     => 'H:i:s',
